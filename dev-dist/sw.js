adf056039112a23f2b01ba116739ec06585f4077
[dyad] <markus.bisang+github@gmail.com>
2026-07-22 15:18:52 +0200

    Update 1 file

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -78,7 +78,7 @@ define(['./workbox-7e5eb42b'], (function (workbox) { 'use strict';
    */
   workbox.precacheAndRoute([{
     "url": "/index.html",
-    "revision": "0.22bifb72sqo"
+    "revision": "0.fvf3ju7l554"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("/index.html"), {
