401aaf0e33fd61c183d832a9aefd7109ee41cbab
[dyad] <markus.bisang+github@gmail.com>
2026-07-22 10:19:49 +0200

    [dyad] React-/next-themes-Abhängigkeitskonflikt beheben

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -78,7 +78,7 @@ define(['./workbox-7e5eb42b'], (function (workbox) { 'use strict';
    */
   workbox.precacheAndRoute([{
     "url": "/index.html",
-    "revision": "0.17ae918jsa"
+    "revision": "0.j03lci7j8h8"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("/index.html"), {
