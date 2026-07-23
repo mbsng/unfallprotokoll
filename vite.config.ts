0e2fd601e7316262d742f3520893243834411c8c
[dyad] <markus.bisang+github@gmail.com>
2026-07-23 13:26:29 +0200

    [dyad] (2 files changed)

--- vite.config.ts
+++ vite.config.ts
@@ -39,8 +39,7 @@ export default defineConfig(() => ({
         skipWaiting: true,
       },
       devOptions: {
-        enabled: true,
-        navigateFallback: "/index.html",
+        enabled: false,
       },
     }),
   ],
